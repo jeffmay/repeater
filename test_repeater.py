cb66d0fe41e4cf2d80677d38247c3b4546df86bb
Jeff May <viralidealogue@gmail.com>
2012-10-17 23:58:26 -0400

    Fixed tests to pass.

--- test_repeater.py
+++ test_repeater.py
@@ -5,6 +5,7 @@ import unittest
 import sys
 from itertools import izip
 from StringIO import StringIO
+from tempfile import TemporaryFile
 
 from repeater import learn, main, repeat_files, repeat_tables
 
@@ -43,11 +44,12 @@ class TestLearnKnowledgeMap(unittest.TestCase):
 class TestRepeaterCLI(unittest.TestCase):
 
     def testSampleData(self):
-        resultIO = StringIO()
-    	repeat_files(open('last_month.csv', 'rb'), open('next_month_input.csv', 'rb'), resultIO)
-        resultData = csv.reader(resultIO.getvalue())
+        resultFile = TemporaryFile()
+    	repeat_files(open('last_month.csv', 'rb'), open('next_month_input.csv', 'rb'), resultFile)
+        resultFile.seek(0)
         with open('next_month_complete.csv', 'rb') as expectedFile:
             expectedData = csv.reader(expectedFile)
+            resultData = csv.reader(resultFile)
             for expected, actual in izip(expectedData, resultData):
                 self.assertEqual(expected, actual)
 
